7064eb2232bccc54a1b683b945a3a37c1b0e3f86
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2019-07-28 09:11:22 +0200

    Initial commit

---
+++ WinAppsLinker/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WinAppsLinker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeViewApps = new System.Windows.Forms.TreeView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.checkBoxMsApps = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // treeViewApps
+            // 
+            this.treeViewApps.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeViewApps.ImageIndex = 0;
+            this.treeViewApps.ImageList = this.imageList;
+            this.treeViewApps.Location = new System.Drawing.Point(12, 36);
+            this.treeViewApps.Name = "treeViewApps";
+            this.treeViewApps.SelectedImageIndex = 0;
+            this.treeViewApps.Size = new System.Drawing.Size(776, 402);
+            this.treeViewApps.TabIndex = 0;
+            this.treeViewApps.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeViewApps_BeforeSelect);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // checkBoxMsApps
+            // 
+            this.checkBoxMsApps.AutoSize = true;
+            this.checkBoxMsApps.Location = new System.Drawing.Point(13, 13);
+            this.checkBoxMsApps.Name = "checkBoxMsApps";
+            this.checkBoxMsApps.Size = new System.Drawing.Size(133, 17);
+            this.checkBoxMsApps.TabIndex = 1;
+            this.checkBoxMsApps.Text = "Show all installed apps";
+            this.checkBoxMsApps.UseVisualStyleBackColor = true;
+            this.checkBoxMsApps.CheckedChanged += new System.EventHandler(this.CheckBoxMsApps_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBoxMsApps);
+            this.Controls.Add(this.treeViewApps);
+            this.Name = "MainForm";
+            this.Text = "Apps Linker";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewApps;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.CheckBox checkBoxMsApps;
+    }
+}
+
---
+++ WinAppsLinker/MainForm.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace WinAppsLinker
+{
+    public partial class MainForm : Form
+    {
+        private class Application
+        {
+            public string ApplicationName { get; }
+            public string ApplicationId { get; }
+            public string ImagePath { get; }
+            public int ImageIndex { get; }
+
+            public Application(string name, string id, string imagePath, int imageIndex)
+            {
+                ApplicationName = name;
+                ApplicationId = id;
+                ImagePath = imagePath;
+                ImageIndex = imageIndex;
+            }
+        }
+
+        private IReadOnlyList<Application> _applications;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PopulateApps()
+        {
+            var applications = new List<Application>();
+
+            // GUID taken from https://docs.microsoft.com/en-us/windows/win32/shell/knownfolderid
+            var FODLERID_AppsFolder = new Guid("{1e87508d-89c2-42f0-8a7e-645a0f50ca58}");
+            ShellObject appsFolder = (ShellObject)KnownFolderHelper.FromKnownFolderId(FODLERID_AppsFolder);
+
+            foreach (var app in (IKnownFolder)appsFolder)
+            {
+                // The friendly app name
+                string name = app.Name;
+                // The ParsingName property is the AppUserModelID
+                string appUserModelID = app.ParsingName; // or app.Properties.System.AppUserModel.ID
+                                                         // You can even get the Jumbo icon in one shot
+
+                imageList.Images.Add(appUserModelID, app.Thumbnail.Bitmap);
+                var imageIndex = imageList.Images.Count - 1;
+
+                applications.Add(new Application(name, appUserModelID, null, imageIndex));
+            }
+
+            _applications = applications;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            imageList.Images.Add("appKey", SystemIcons.Application);
+
+            PopulateApps();
+            PopulateTreeApps(checkBoxMsApps.Checked);
+        }
+
+        private Regex _microsoftStoreIdPattern = new Regex(@"[\w\._\-]+![\w\._\-]+", RegexOptions.Compiled);
+
+        private void PopulateTreeApps(bool showAllApps)
+        {
+            treeViewApps.Nodes.Clear();
+            treeViewApps.Nodes.AddRange(
+                _applications
+                    .Where(a => showAllApps ? true : _microsoftStoreIdPattern.IsMatch(a.ApplicationId))
+                    .OrderBy(a => a.ApplicationName)
+                    .Select(CreateNodeFromApplication)
+                    .ToArray());
+        }
+
+        private TreeNode CreateNodeFromApplication(Application app)
+        {
+            var node = new TreeNode(app.ApplicationName, app.ImageIndex, app.ImageIndex);
+
+            var id = app.ApplicationId;
+            if (_microsoftStoreIdPattern.IsMatch(id))
+                id = @"shell:AppsFolder\" + id;
+
+            var subNode = node.Nodes.Add("id", id);
+            subNode.ImageIndex = 0;
+            node.Collapse();
+
+            return node;
+        }
+
+        /// <summary>
+        /// Copy the selection text in the clipboard
+        /// </summary>
+        private void TreeViewApps_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node == null)
+                return;
+
+            Clipboard.SetText(e.Node.Text);
+        }
+
+        private void CheckBoxMsApps_CheckedChanged(object sender, EventArgs e)
+        {
+            PopulateTreeApps(checkBoxMsApps.Checked);
+        }
+    }
+}
